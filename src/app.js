09fd6b4d33fc45826f30ce8288ff719b603f55d0
Sagar Subedi <sagarxic@gmail.com>
2025-04-02 00:43:21 +0545

    assign svg+xml content type to extensionless files

--- src/app.js
+++ src/app.js
@@ -2,17 +2,48 @@ const express = require("express");
 const fs = require("fs");
 const path = require("path");
 const cors = require("cors");
+const mime = require('mime-types');
+
 
 const app = express();
 const coursesDir = path.join(__dirname, "../public/courses");
+const staticDir = path.join(__dirname, '../public');
 
-app.use(express.static("public")); // Serve static files
 // app.use(cors({ origin: "http://localhost:5173" })); // Allow React dev server
 app.use(cors());
 
 // ✅ Allow JSON and URL Encoded Data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+
+// Serve static files
+app.use(express.static(staticDir,{
+    
+    
+    setHeaders: (res, filePath) => {
+
+    
+    //   fs.readFile(path, 'utf8', (err, data) => {
+    //     if (err) {
+    //       console.error('Error reading file:', err);
+    //       return;
+    //     }
+    //     // Check if the content contains the SVG opening tag
+    //     if (data.startsWith('<svg')) {
+    //       res.setHeader('Content-Type', 'image/svg+xml; charset=UTF-8');
+    //     } 
+    //   });
+
+    //Assuming the file is an SVG if it has no extension, the logic above
+    //is performance wise inefficient as that requires reading the file
+    if (!path.extname(filePath)) {  // Check if the file has no extension
+        res.setHeader('Content-Type', 'image/svg+xml; charset=UTF-8');
+    }
+
+    }
+  }));
+
 
 // List all available courses
 app.get("/courses", (req, res) => {
@@ -26,7 +57,6 @@ app.get("/courses", (req, res) => {
 // List all lessons in a selected course
 app.get("/courses/:courseName/lessons", (req, res) => {
     const coursePath = path.join(coursesDir, req.params.courseName, "lessons_html");
-    
     fs.readdir(coursePath, (err, files) => {
         if (err) return res.status(500).json({ error: "Cannot read course directory" });
         const lessons = files.filter(file => file.endsWith(".html")).map(file => file.replace(".html", ""));
